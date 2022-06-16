fa7be5cc03778b05e21f0b134048d105daed9ad5
dainahwanjiku <deeshish2017@gmail.com>
2022-06-16 17:05:10 +0300

    files

---
+++ instruct_mul.c
@@ -0,0 +1,22 @@
+#include "monty.c"
+
+/**
+ * instruct_mul - multiplies second top node from top node
+ * @stack: double pointer to top of stack
+ * @line_number: value of node
+ *
+ * Return: void
+ */
+void instruct_mul(stack_t **stack unsigned int line_number)
+{
+	int *tmp;
+
+	if(var. < 2)
+	{
+		printf("L%u: can't mul, stack too short", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = (*stack)->n;
+	instruct_pop(stack, line_number);
+	(*stack)->n *= tmp;
