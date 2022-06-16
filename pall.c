a66aea3432f91396f239b6c5f243b3cedd207023
dainahwanjiku <deeshish2017@gmail.com>
2022-06-16 04:00:27 +0300

    monty files

---
+++ pall.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * pall - prints all the values on the stack
+ * @stack: double pointer to head of stack
+ * @line_number: line number executed from script file
+ *
+ * Return: void
+ */
+void instruct_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head;
+	
+	(void)(line_number);
+
+	heaad = *stack;
+	while (head != NULL)
+	{
+		printf("%d\n", head->n);
+		head = head->next;
+		if (head == *stack)
+		{
+			return;
+		}
+	}
+}
