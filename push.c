796f4e2092dddd1c601d8da40461bde40284f12a
dainahwanjiku <deeshish2017@gmail.com>
2022-06-16 03:19:51 +0300

    monty files

---
+++ push.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+
+/**
+ * push - pushes the element to stack
+ * @stack: double pointer to the first node
+ * @line_number: value of new node
+ *
+ * Return: (0)
+ */
+void instruct_push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *element = malloc(sizeof(stack_t));
+	char *op;
+	int num;
+
+	if(element == NULL)
+	{
+		printf("Error: malloc failed!\n");
+		exit(EXIT_FAILURE);
+	}
+	op = strtok(NULL, DELIMS);
+	if (op == NULL || stack == NULL)
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	num = _strtol(op, line_number);
+	element->n = num;
+	element->prev = null;
+	element->next = *stack;
+	if (element->next != NULL)
+		(element->next)->prev = element;
+	*stack = element;
+}
