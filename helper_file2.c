142d75c9d58b820a5223fee780c578fab0cdb079
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-06-17 23:04:12 +0300

    Update helper_file2.c

--- helper_file2.c
+++ helper_file2.c
@@ -11,21 +11,21 @@ void delegate_op(stack_t **stack, char *op, unsigned int line_number)
 {
 	int i = 0;
 	instruction_t all_ops[] = {
-		{"push", instruction_push},
-		{"pall", instruction_pall},
-		{"pint", instruction_pint},
-		{"pop", instruction_pop},
-		{"swap", instruction_swap},
-		{"add", instruction_add},
-		{"nop", instruction_nop},
-		{"sub", instruction_sub},
-		{"div", instruction_div},
-		{"mul", instruction_mul},
-		{"mod", instruction_mod},
-		{"pchar", instruction_pchar},
-		{"pstr", instruction_pstr},
-		{"rotl", instruction_rotl},
-		{"rotr", instruction_rotr},
+		{"push", op_push},
+		{"pall", op_pall},
+		{"pint", op_pint},
+		{"pop", op_pop},
+		{"swap", op_swap},
+		{"add", op_add},
+		{"nop", op_nop},
+		{"sub", op_sub},
+		{"div", op_div},
+		{"mul", op_mul},
+		{"mod", op_mod},
+		{"pchar", op_pchar},
+		{"pstr", op_pstr},
+		{"rotl", op_rotl},
+		{"rotr", op_rotr},
 		{NULL, NULL}
 	};
 
