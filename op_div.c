b8e83a711736e07cfe821b22a58ce2e6425631e9
dainahwanjiku <deeshish2017@gmail.com>
2022-06-19 22:22:49 +0300

    files

--- op_div.c
+++ op_div.c
@@ -10,19 +10,19 @@
  */
 void op_div(stack_t **stack, unsigned int line_number)
 {
-	int *tmp;
+	int *n;
 
 	if(var.stack_len < 2)
 	{
 		printf("L%u: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	tmp = (*stack)->n;
-	if (tmp == 0)
+	n = (*stack)->n;
+	if (n == 0)
 	{
 		printf("L%u: division by zero\n", line_number);
 		exit(EXIT_FAILURE)
 	}
 	op_pop(stack, line_number);
-	(*stack)->n /= tmp;
+	(*stack)->n /= n;
 }
