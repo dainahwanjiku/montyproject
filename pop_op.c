d4d7ca2995603c7c6e7ed3d84e683ce396f72875
dainah <99591839+dainahwanjiku@users.noreply.github.com>
2022-06-20 01:52:59 +0300

    Update and rename op_pop.c to pop_op.c

--- pop_op.c
+++ pop_op.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 
 /**
- * instruct_pop - pop top element off of `stack'
+ * pop - pop top element off of `stack'
  * @stack: double pointer to head of stack
  * @line_number: line number of current operation
  *
  * Return: void
  */
-void op_pop(stack_t **stack, unsigned int line_number)
+void pop_op(stack_t **stack, unsigned int line_number)
 {
 	stack_t *pop = *stack;
 
