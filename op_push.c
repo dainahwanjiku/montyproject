67ef7480a50587257a2cddac7c5cc9a3f18d6e8f
dainahwanjiku <deeshish2017@gmail.com>
2022-06-20 02:23:17 +0300

    files

--- op_push.c
+++ op_push.c
@@ -1,34 +1,51 @@
 #include "monty.h"
+#include <ctype.h>
 
 /**
- * push - pushes the element to stack
- * @stack: double pointer to the first node
- * @line_number: value of new node
+ * check_for_digit - checks that a string only contains digits
+ * @arg: string to check
+ *
+ * Return: 0 if only digits, else 1
+ */
+static int check_for_digit(char *arg)
+{
+	int i;
+
+	for (i = 0; arg[i]; i++)
+	{
+		if (arg[i] == '-' && i == 0)
+			continue;
+		if (isdigit(arg[i]) == 0)
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * m_push - push an integer onto the stack
+ * @stack: double pointer to the beginning of the stack
+ * @line_number: script line number
  *
  * Return: void
  */
-void op_push(stack_t **stack, unsigned int line_number)
+void m_push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *element = malloc(sizeof(stack_t));
-	char *op;
-	int num;
+	char *arg;
+	int n;
 
-	if(element == NULL)
+	arg = strtok(NULL, "\n\t\r ");
+	if (arg == NULL || check_for_digit(arg))
 	{
-		printf("Error: malloc failed!\n");
+		dprintf(STDOUT_FILENO,
+			"L%u: usage: push integer\n",
+			line_number);
 		exit(EXIT_FAILURE);
 	}
-	op = strtok(NULL, DELIMS);
-	if (op == NULL || stack == NULL)
+	n = atoi(arg);
+	if (!add_node(stack, n))
 	{
-		printf("L%u: usage: push integer\n", line_number);
+		dprintf(STDOUT_FILENO, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	num = _strtol(op, line_number);
-	element->n = num; /* linked list used here */
-	element->prev = null;
-	element->next = *stack;
-	if (element->next != NULL)
-		(element->next)->prev = element;
-	*stack = element;
+	var.stack_len++;
 }
